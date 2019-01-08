6b72c4ebdc3981c4e2cfe8a26688ee7df54b25a1
TNtLike <40592693+TNtLike@users.noreply.github.com>
2019-01-08 16:02:30 +0800

    Update SingleBlogItem.vue

--- src/components/SingleBlogItem.vue
+++ src/components/SingleBlogItem.vue
@@ -70,6 +70,39 @@
         </el-col>
       </el-row>
     </div>
+    <footer class="footerItem">
+      <hr>
+      <el-row>
+        <el-col :span="6" :offset="4">
+          <div class="leftItem">
+            <h3>LINKS</h3>
+            <!-- <font-awesome-icon icon="fab fa-github"/> -->
+            <i class="fab fa-github"></i>
+            <a>GitHub</a>
+            <br>
+            <i class="fab fa-weibo"></i>
+            <a>Weibo</a>
+          </div>
+        </el-col>
+        <el-col :span="6" :offset="4">
+          <div class="rightItem">
+            <h3>联系我</h3>
+            <p>
+              Tel:
+              +86 1375 555 5555
+            </p>
+            <p>
+              Email:
+              qiulangcheng@gmail.com
+            </p>
+            <p>
+              Loc:
+              Earth
+            </p>
+          </div>
+        </el-col>
+      </el-row>
+    </footer>
   </div>
 </template>
 
@@ -162,7 +195,15 @@ header {
 article {
   padding: 10px;
 }
-h3 {
-  margin: 40px 0 0;
+footer {
+  height: 200px;
+  width: 100vm;
+  background: #f8f8f9;
+}
+.leftItem {
+  color: #303133;
+}
+.rightItem {
+  color: #303133;
 }
 </style>
