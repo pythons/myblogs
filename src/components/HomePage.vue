95905e2953e8cb03a64111c76272b15d75c1fa82
TNtLike <40592693+TNtLike@users.noreply.github.com>
2019-01-28 14:42:25 +0800

    1

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="homepage">
+    <test/>
     <header-item/>
     <show-blogs-item/>
     <footer-item/>
@@ -7,12 +8,14 @@
 </template>
 
 <script>
+import test from "./1111";
 import headerItem from "./HeaderItem.vue";
 import showBlogsItem from "./ShowBlogsItem.vue";
 import footerItem from "./FooterItem.vue";
 export default {
   name: "homepage",
   components: {
+    test,
     headerItem,
     showBlogsItem,
     footerItem
