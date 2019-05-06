b0abe9893aa2f75d072d19734047c6ba866bb282
TNtLike <qiulangcheng@gmail.com>
2019-05-06 15:18:32 +0800

    Update SignUpItem.vue

--- src/components/SignUpItem.vue
+++ src/components/SignUpItem.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="signUpItem">
     <div class="item">
-      <!-- <el-row :gutter="20">
-      <el-col :span="8" :offset="8">-->
-      <el-form label-width="120px" :model="user">
-        <el-form-item label="请输入用户名">
-          <el-input v-model="user.username" placeholder="请输入用户名" clearable></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱">
-          <el-input v-model="user.email" placeholder="请输入邮箱" clearable></el-input>
-        </el-form-item>
-        <el-form-item label="请输入密码">
-          <el-input type="password" v-model="user.password" placeholder="请输入密码" clearable></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码">
-          <el-input type="password" v-model="repassword" placeholder="请确认密码" clearable></el-input>
-        </el-form-item>
-        <el-form-item label="密码保护问题">
-          <!-- <el-input v-model="user.question" placeholder="请输入密码" clearable></el-input> -->
-          <el-select v-model="user.question" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="答案">
-          <el-input v-model="user.answer" placeholder="请输入答案" clearable></el-input>
-        </el-form-item>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="个人注册" name="1"></el-tab-pane>
+        <el-tab-pane label="企业注册" name="2"></el-tab-pane>
+      </el-tabs>
+      <div v-show="activeName=='1'">
+        <el-form label-width="120px" :model="user">
+          <el-form-item label="用户名">
+            <el-input v-model="user.username" placeholder="请输入用户名" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="user.email" placeholder="请输入邮箱" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input type="password" v-model="user.password" placeholder="请输入密码" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码">
+            <el-input type="password" v-model="repassword" placeholder="请确认密码" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="密码保护问题">
+            <el-select v-model="user.question" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="答案">
+            <el-input v-model="user.answer" placeholder="请输入答案" clearable></el-input>
+          </el-form-item>
 
-        <el-form-item>
-          <el-button type="success" @click="submit">提交</el-button>
-          <el-button type="primary" @click="back">返回</el-button>
-        </el-form-item>
-      </el-form>
+          <el-form-item>
+            <el-button type="primary" @click="submit">提交</el-button>
+            <el-button @click="back">返回</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
       <!-- </el-col>
       </el-row>-->
     </div>
@@ -48,6 +51,7 @@ export default {
   name: "signUpItem",
   data() {
     return {
+      activeName: "1",
       user: {
         username: "",
         password: "",
